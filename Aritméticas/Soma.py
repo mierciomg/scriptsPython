4481bfb2c7fc01c8c7a8d83cc495dae73c01f0e3
Miércio Martinelli <mierciomg@gmail.com>
2023-08-17 14:25:36 -0300

    atualizacao

--- Aritméticas/Soma.py
+++ Aritméticas/Soma.py
@@ -1,4 +1,16 @@
 n1 = int(input('informe um numero'))
 n2 = int(input('informe outro número'))
 soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+potencia = n1 ** n2
+divisaoExata = n1 // n2
+restoDaDivisao = n1 % n2
 print('A soma entre {} e {} é igual a {}'.format(n1, n2, soma))
+print('A subtração entre {} e {} é igual a {}'.format(n1, n2, subtracao))
+print('A multiplicação {} e {} é igual a {}'.format(n1, n2, multiplicacao))
+print('A divisão {} e {} é igual a {}'.format(n1, n2, divisao))
+print('{} elevado a {} é igual a {}'.format(n1, n2, potencia))
+print('A divisão exata de {} por {} é igual a {}'.format(n1, n2, divisaoExata))
+print('O resto da divisão de {} por {} é igual a {}'.format(n1, n2, restoDaDivisao))
